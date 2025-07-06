24c05863d3443b10d6697efaaa56ceb9d75021ec
JamieBliss <zfac223@live.rhul.ac.uk>
2025-07-06 09:52:18 +0100

    Added button to go directly to results page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { FileInput } from "@/components/ui/fileInput";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/results");
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[18px] row-start-2 items-center sm:items-start">
         <FileInput />
+        <Button size="sm" className="w-full" onClick={handleClick}>
+          View Existing Data
+        </Button>
       </main>
     </div>
   );
