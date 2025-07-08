5c70ab0d469cfb9b0466822d54d8a002a29980ec
JamieBliss <zfac223@live.rhul.ac.uk>
2025-07-08 12:15:36 +0100

    Fixed pagination

--- app/results/data-table.tsx
+++ app/results/data-table.tsx
@@ -56,6 +56,7 @@ export function DataTable({ data, columns, employee_sizes }: DataTableProps) {
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
+    getPaginationRowModel: getPaginationRowModel(),
     state: {
       sorting,
       columnVisibility,
